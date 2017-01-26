03a56b3140c9f49f950b9d63966e957609bc810f
Jesse Sanders <jesse.sanders@briebug.com>
2017-01-26 06:48:26 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {TranslateModule} from 'ng2-translate/ng2-translate';
+import {ToastModule} from 'ng2-toastr/ng2-toastr';
+import {CookieService} from 'angular2-cookie/services/cookies.service';
+
+import {routing} from './app.routes';
+
+import {AppComponent} from './app.component';
+import {LoginModule} from './components/login/login.module';
+import {SessionService} from './services';
+import { MyComponentComponent } from './components/my-component/my-component.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        MyComponentComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        routing,
+        TranslateModule.forRoot(),
+        ToastModule,
+        LoginModule
+    ],
+    providers: [
+        CookieService,
+        SessionService
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
